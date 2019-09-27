9b7520423b45ca0e1929a08fbd7894e65038a2a6
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2019-09-26 20:28:24 -0500

    Add index js and some component directory stuff

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+export default
+`<header id="test-id" class="test-class">
+<i class="fas fa-cat"></i>
+<figure>
+  <img
+    srcset="
+      https://picsum.photos/200,
+      https://picsum.photos/300 1.5x,
+      https://picsum.photos/400 2x,
+      https://picsum.photos/600 3x
+    "
+    src="https://picsum.photos/600"
+    alt="Lorem Picsum"
+  />
+</figure>
+<h1>Vanessa Kratohvil Practice Project</h1>
+</header>`
